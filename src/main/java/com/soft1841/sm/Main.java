f34985549b70cea911f4b874b2d3667939083994
杨苏祥 <3597107837@qq.com>
2018-12-23 17:03:24 +0800

    my gitdemo

--- src/main/java/com/soft1841/sm/Main.java
+++ src/main/java/com/soft1841/sm/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +12,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("登陆");
-        primaryStage.setScene(new Scene(root, 756, 458));
+        primaryStage.setTitle("超市管理界面");
+       primaryStage.setScene(new Scene(root, 1920, 1020));
         primaryStage.show();
     }
 
